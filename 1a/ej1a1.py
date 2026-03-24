b1cb70c727aa293501d0b498a246ef466ecbe0d3
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-03-24 13:23:10 +0100

    Borra enunciat en castella

--- 1a/ej1a1.py
+++ 1a/ej1a1.py
@@ -1,23 +1,4 @@
 '''
-Enunciado:
-Implementa la función 'fibonacci(fibonacci_number)' que contenga el algoritmo
-de Fibonacci y reciba como parámetro un valor numérico entero llamado 
-'fibonacci_number'  y devuelva el valor de la serie Fibonacci en esa posición.
-Asimismo, si el valor no es numérico, o es menor a cero, se debe lanzar 
-una excepción ValueError("mensaje"), la cual puede incluir un mensaje que debería 
-corresponder con el tipo de error durante la validación.
-
-Parámetros:
-- fibonacci_number: Número entero positivo mayor a 0 que representa la
-posición en la serie Fibonacci.
-
-Ejemplo:
-    Entrada:
-    fibonacci(10)
-
-    Salida:
-    55
-
 Enunciat:
 Implementa la funció 'fibonacci(fibonacci_number)' que contingui l'algorisme
 de Fibonacci i rebi com a paràmetre un valor numèric enter anomenat
