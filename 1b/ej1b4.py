8a584f7e4496726bb417b34efeafd21cf7f4002a
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-03-27 13:52:09 +0100

    Preparem tots els enunciats

--- 1b/ej1b4.py
+++ 1b/ej1b4.py
@@ -1,34 +1,4 @@
 '''
-Enunciado:
-Implementar la función 'results(list_numbers)' que reciba como parámetro una
-lista que contiene números enteros y decimales llamada 'list_numbers', se pide
-calcular el promedio y la desviación estándar mediante la
-librería "Numpy" de todos los números dentro de la lista.
-
-Cada resultado se debe imprimir en pantalla con su respectivo nombre 
-'Average: {value}' y 'Standard deviation: {value}', dichos resultados deben
-estar redondeados a 2 decimales. 
-
-Los resultados también han de ser devueltos por la función.
-
-Puedes consultar la referencia de la librería 'Numpy' en el siguiente enlace:
-https://numpy.org/doc/stable/reference/index.html#reference
-
-En concreto, te recomendamos que consultes:
-* https://numpy.org/doc/stable/reference/generated/numpy.mean.html
-* https://numpy.org/doc/stable/reference/generated/numpy.std.html#numpy.std
-
-Parámetro:
-- list_numbers: Lista de números enteros y decimales.
-
-Ejemplo:
-    Entrada:
-    [1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42]
-    
-    Salida:
-    Average: 19.74
-    Standard deviation: 26.03
-
 Enunciat:
 
 Implementar la funció 'results(list_numbers)' que rebi com a paràmetre una
@@ -68,6 +38,5 @@ def results(list_numbers):
     # Write here your code
     pass
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])
