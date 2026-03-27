193cb78f7389ff2b5d2bb30123241842be2d7e1e
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-03-27 12:43:09 +0100

    exercici 3 fet

--- 1a/ej1a3.py
+++ 1a/ej1a3.py
@@ -1,19 +1,4 @@
 '''
-Enunciado:
-Implementa una función llamada "invert_text(text_chain)" que reciba como
-parámetro una cadena de texto de tipo string llamada 'text_chain' y devuelva
-el texto invertido.
-
-Parámetros:
-- text_chain: Este parámetro solo admite strings.
-
-Ejemplo:
-    Entrada:
-    invert_text('Hello world!')
-
-    Salida:
-    !dlrow olleH
-
 Enunciat:
 Implementa una funció anomenada "invert_text(text_chain)" que rebi com
 paràmetre una cadena de text de tipus string anomenada 'text_chain' i torni
@@ -32,9 +17,7 @@ Exemple:
 '''
 
 def invert_text(text_chain:str):
-    # Write here your code
-    pass
-
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
+    return text_chain[::-1]
+ 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(invert_text("Hello world!"))
+print(invert_text("Hello world!"))
