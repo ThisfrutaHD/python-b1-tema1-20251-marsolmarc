259fb6bf846c86b1c691418f464da66cc69618af
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-03-27 13:45:28 +0100

    Exercici acabat

--- 1b/ej1b2.py
+++ 1b/ej1b2.py
@@ -1,36 +1,4 @@
 """
-Enunciado:
-Utilizando la librería 'math', implementa una función llamada 
-'calculate_angle(angle)'  que reciba como parámetro un número 
-correspondiente a un ángulo en grados llamado 'angle' y retorne
-como resultado el seno de dicho ángulo redondeado a 2 decimales.
-
-Encontrarás la documentación de la librería 'math' en el 
-siguiente enlace: https://docs.python.org/3/library/math.html
-
-En concreto, la función 'sin(x)' de la librería 'math' la 
-puedes encontrar en el siguiente enlace:
-https://docs.python.org/3/library/math.html#math.sin
-
-Y la función 'radians(x)' de la librería 'math' la puedes
-encontrar en el siguiente enlace:
-https://docs.python.org/3/library/math.html#math.radians
-(te servirá para convertir los grados a radianes)
-
-Recuerda que puedes redondear decimales con la función 
-'round(x, n)'
-
-
-Parámetros:
-- angle: Entero correspondiente al valor de un ángulo.
-
-Ejemplo:
-    Entrada:
-    calculate_angle(270)
-
-    Salida:
-    -1
-
 Enunciat:
 Utilitzant la llibreria 'math', implementa una funció anomenada
 'calculate_angle(angle)' que rebi com a paràmetre un número
@@ -53,7 +21,7 @@ Recorda que pots arrodonir decimals amb la funció
 'round(x, n)'
 
 Paràmetres:
-- àngel: Sencer corresponent al valor d'un angle.
+- angle: Sencer corresponent al valor d'un angle.
 
 Exemple:
      Entrada:
@@ -63,12 +31,12 @@ Exemple:
      -1
 """
 
-import math
+import math as m
 
 def calculate_angle(angle):
-    # Write here your code
-    pass
+    sinus = m.sin(m.radians(angle))
+    return sinus
+    
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(calculate_angle(270))
+#print(calculate_angle(270))
