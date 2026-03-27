4314229c24cb252bf4473535d2cfb9ca7e8dfb8f
ThisfrutaHD <268890942+ThisfrutaHD@users.noreply.github.com>
2026-03-27 13:34:04 +0100

    Fet

--- 1b/ej1b1.py
+++ 1b/ej1b1.py
@@ -1,21 +1,4 @@
 """
-Enunciado:
-Implementar la función 'obtain_max(list_numbers)' que recibe 
-como parámetro una lista no vacía de números enteros y devuelve
-el número mayor de la lista.
-
-Recuerda que en Python existe la función llamada 'max'
-
-Parámetros:
-- list_numbers: Lista de números enteros
-
-Ejemplo:
-    Entrada:
-    obtain_max([1, 45, 87, 21, 0, 23, 28])
-
-    Salida:
-    87
-
 Enunciat:
 Implementar la funció 'obtain_max(list_numbers)' que rep 
 com a paràmetre una llista no buida de nombres enters i retorna
@@ -35,9 +18,7 @@ Exemple:
 """
 
 def obtain_max(list_numbers):
-    # Write here your code
-    pass
+    return max(list_numbers)
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 # print(obtain_max([1, 45, 87, 21, 0, 23, 28]))
